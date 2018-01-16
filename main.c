12ab59013a7ea7e6f886f3a33cddc8f452156e32
Mimerme <androssdragonq.9@gmail.com>
2018-01-16 18:40:36 -0500

    Implemnted quickstart option

--- main.c
+++ main.c
@@ -7,15 +7,13 @@
 typedef char* string;
 char* filename;
 
-void compute_filepath(){
+void compute_filepath(string file){
     string homePath = getenv("HOME");
-    filename = malloc(strlen(homePath) + strlen("/.cheatsheet/.sheets"));
-    filename = homePath;
+    filename = malloc(strlen(homePath) + strlen("/.cheatsheet/") + strlen(file) + 1);
+    memcpy(filename, homePath, strlen(homePath));
     strcat(filename, "/.cheatsheet/");
     mkdir(filename, 0775);
-    strcat(filename, ".sheets");
-
-    free(filename);
+    strcat(filename, file);
 }
 
 //Checks if the file is NULL
@@ -24,6 +22,14 @@ void check_file(FILE *name){
         printf("Problem opening the file. Does the file exist?\n");
         exit(0);
     }
+}
+
+int has_quickstart(){
+    compute_filepath("Quickstart");
+    if(fopen("r", filename) != NULL){
+        return 1;
+    }
+    return 0;
 }
 
 void execute_process(string bin_path, string file_path){
@@ -37,17 +43,9 @@ void execute_process(string bin_path, string file_path){
     free(new_str);
 }
 
-int quickstart(){
-    //Check if PQIV is installed
-    //Check if AUTOhotkey is installed for windows
-#ifdef _WIN32
 
-#endif
-    //xbindkeys for linux
-#ifdef __linux__
-#endif
+//-1 error
 
-}
 
 long get_file_size(FILE *file){
    check_file(file);
@@ -91,7 +89,7 @@ void buffer_split(string buffer, const char split_char, string splits[]){
 
 //Define the functions
 void list_sheets(string args[]){
-    compute_filepath();
+    compute_filepath(".sheets");
 
     //.sheets contains a mapping to all of the saved cheatsheets and their locations
     FILE *saved_sheets;
@@ -143,7 +141,7 @@ void add_binary(int argc, string args[]){
         return;
     }
     
-    compute_filepath();
+    compute_filepath(".sheets");
 
     char *binary_location = args[2];
     char *binary_type = args[3];
@@ -156,12 +154,62 @@ void add_binary(int argc, string args[]){
    
 }
 
+int quickstart(){
+    
+    //Check if PQIV is installed
+    int pqiv = system("pqiv&> /dev/null");
+    
+    if(pqiv == 127){
+        printf("PQIV is not installed");
+        return -1;
+    }
+    else if(pqiv == -1){
+        printf("There was an issue with PQIV");
+        return -1;
+    }
+    //Check if AUTOhotkey is installed for windows
+#ifdef _WIN32
+
+#endif
+    //xbindkeys for linux
+#ifdef __linux__
+    int xkeys = system("xbindkeys&> /dev/null");
+
+    if(xkeys == 127){
+        printf("xbindkeys is not installed");
+        return -1;
+    }
+    else if(xkeys == -1){
+        printf("There was an issue with xbindkeys");
+        return -1;
+    }
+#endif
+    //if everythinng is configured make a file in .cheatsheet
+
+    printf("Creating a file called `Quickstart` to automatically ask the user to bind keys when adding sheets. Delete this if you want to use something else other than xbindkeys\n");
+    compute_filepath("Quickstart");
+    fopen(filename, "r");
+
+    string args[] = {"", "", "pqiv", "pdf"};
+    add_binary(4, args);
+    args[3] = "img";
+    add_binary(4, args);
+
+    //otherwise run the script corresponding to the OS
+#ifdef __linux__
+#endif
+
+#ifdef _WIN32
+#endif
+
+}
+
 void add_sheets(int argc, string args[]){
     if(argc != 5){
         printf("Specifed %i parameters when there should be 3\n", argc - 2);
         return;
     }
-    compute_filepath();
+    compute_filepath(".sheets");
 
     char *sheet_name = args[2];
     char *sheet_path = args[3];
@@ -172,6 +220,35 @@ void add_sheets(int argc, string args[]){
     fprintf(saved_sheets, "%s:%s:%s\n", sheet_name, sheet_path, sheet_type);
 
     fclose(saved_sheets);
+
+#ifdef __linux__
+    //Assistant to bind keys
+    if(has_quickstart() == 1){
+       FILE *xbind = fopen("~/.xbindkeysrc", "a+");
+       FILE *keys_output= popen("xbindkeys -k", "r");
+        
+       int out_size = get_file_size(keys_output);
+       char out_buff[out_size];
+
+       fread(out_buff, sizeof(char), out_size, keys_output);
+       int newlines = count_chars(out_buff, out_size, 10);
+
+       string splits[newlines];
+       buffer_split(out_buff, 10, splits);
+
+       //Create the string that we are printing to ~/.xbindkeysrc
+       string tmp;
+       tmp = malloc(strlen("\"cheatsheet show ") + strlen(sheet_name) + strlen("\"") + 1);
+       strcat(tmp, "\"cheatsheet show");
+       strcat(tmp, sheet_name); 
+       strcat(tmp, "\"");
+
+       fprintf(xbind, tmp);
+       fprintf(xbind, splits[newlines - 1]);
+       fprintf(xbind, "\n\n");
+       free(tmp);
+    }
+#endif
 }
 
 void parse_and_execute(string lines[], int num_lines, string key){
@@ -214,11 +291,11 @@ void show_sheet(int argc, string args[]){
         printf("Specifed %i parameters when there should be 1\n", argc - 2);
         return;
     }
-    compute_filepath();
+    compute_filepath(".sheets");
 
     FILE *saved_sheets;
-    saved_sheets = fopen(filename, "r");
- 
+    saved_sheets = fopen(filename, "r"); 
+
     //Iterate over buffer
     int file_size;
     file_size = get_file_size(saved_sheets);
@@ -272,8 +349,14 @@ int main(int argc, string argv[]){
         case 'b':
             add_binary(argc, argv);
             break;
+        //quickstart configuration
+        case 'q':
+            quickstart();
+            break;
         default:
             printf("Unrecognized command. Type 'help' for more\n");
             break;
     }
+
+    free(filename);
 }
